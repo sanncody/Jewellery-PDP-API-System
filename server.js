323a97b1697867827cf01e8e24f47c209153c090
Sandeep <sandeepwadhawan2001@gmail.com>
2026-01-18 23:57:18 +0530

    Basic express server app is ready

---
+++ server.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
